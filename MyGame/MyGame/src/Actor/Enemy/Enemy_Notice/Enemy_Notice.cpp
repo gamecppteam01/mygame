2b3016c7e849ca2bd1b093f1751f915be7293e33
G016A1331\yoshiki <raikinex430@gmail.com>
2017-12-20 16:42:25 +0900

    スポットライトを狙う敵の追加

---
+++ MyGame/MyGame/src/Actor/Enemy/Enemy_Notice/Enemy_Notice.cpp
@@ -0,0 +1,160 @@
+#include "Enemy_Notice.h"
+#include"../../../World/IWorld.h"
+#include"../../../ScoreManager/ScoreManager.h"
+#include"../../../ScoreManager/ScoreMap.h"
+#include"../../../Actor/Player/Player.h"
+#include"../../../Math/Random.h"
+#include"../../../Sound/TempoManager.h"
+#include"../EnemyBullet.h"
+
+Enemy_Notice::Enemy_Notice(IWorld * world, const std::string & name, const Vector3 & position, int playerNumber, const IBodyPtr & body)
+	: BaseEnemy(world, name, position, playerNumber, body, MODEL_ID::BALANCEENEMY_MODEL, MODEL_ID::BALANCEENEMY_BULLET_MODEL), nextPoint_(0), nextPosition_(position), isGoBonus_(false) {
+	roundPoint_ = world_->getCanChangedScoreMap().getRoundPoint();
+	nextPoint_ = getNearestPoint(centerPosition_);
+	state_ = Notice_State::Normal;
+}
+
+void Enemy_Notice::onMessage(EventMessage message, void * param) {
+	switch (message)
+	{
+	case EventMessage::Lighting:
+		state_ = Notice_State::Steal;
+		nextPosition_ = (Vector3&)param;
+		break;
+	case EventMessage::Extinction:
+		state_ = Notice_State::Normal;
+		setNextPosition();
+		break;
+	}
+}
+
+void Enemy_Notice::JustStep() {
+	nonTargetResetTimer_.Action();
+	if (!isCanStep())return;
+
+	ActorPtr act = getNearestActor();
+	//攻撃射程圏内なら
+	if (Vector2::Distance(Vector2(getNearestActor()->position().x, getNearestActor()->position().z), Vector2(centerPosition_.x, centerPosition_.z)) <= attackDistance&&
+		prevHitActorNumber_ != act->getCharacterNumber()) {
+		change_State_and_Anim(Enemy_State::Attack, Enemy_Animation::Spin);
+		return;
+	}
+
+	//通常時は6拍子毎
+	int rhythmTime = 6;
+	if (world_->getScoreManager().GetCharacterScoreRate(playerNumber_) > 1.0f) {
+		//ボーナス時は3拍子毎
+		rhythmTime = 3;
+	}
+
+	rhythmTimeCount_++;
+	if (rhythmTimeCount_ < rhythmTime) {
+		if (isGoBonus_ && state_ == Notice_State::Normal) {
+			setNextPosition();
+		}
+		return;
+	}
+
+	rhythmTimeCount_ = 0;
+	if (state_ != Notice_State::Normal) return;
+	int r = Random::GetInstance().Range(0, 9);
+	if (r < probability_) {
+		//ターン
+		world_->getCanChangedScoreManager().addScore(playerNumber_, SCORE_TURN);
+		change_State_and_Anim(Enemy_State::Step, Enemy_Animation::Turn, false);
+	}
+	else {
+		//クォーター
+		world_->getCanChangedScoreManager().addScore(playerNumber_, SCORE_QUARTER);
+		change_State_and_Anim(Enemy_State::Step, Enemy_Animation::Quarter, false);
+	}
+}
+
+void Enemy_Notice::updateNormal(float deltaTime) {
+	//3拍目は動かない
+	if (world_->getCanChangedTempoManager().getBeatCount() % 3 == 2)return;
+
+	rotation_ *= Matrix::CreateFromAxisAngle(rotation_.Up(), -5.0f);
+
+	velocity_ += (nextPosition_ - centerPosition_).Normalize()*movePower;
+	gravity_ -= 0.05f;
+
+	Vector3 jumpVector = Vector3(0.0f, gravity_, 0.0f);
+	velocity_ += jumpVector;
+
+	Vector2 myPos;
+	Vector2 pointPos;
+	switch (state_)
+	{
+	case Enemy_Notice::Normal:
+		myPos = Vector2(centerPosition_.x, centerPosition_.z);
+		pointPos = Vector2(nextPosition_.x, nextPosition_.z);
+
+		//ポイントに到達したら
+		if (Vector2::Distance(myPos, pointPos) <= 10.0f && !isGoBonus_) {
+			setNextPosition();
+		}
+		break;
+	case Enemy_Notice::Steal:
+		if (data_->notice_ == true) {
+			change_State(Enemy_State::Fever, Enemy_Animation::Idle);
+		}
+		break;
+	}
+}
+
+void Enemy_Notice::updateFever(float deltaTime) {
+	rotation_ *= Matrix::CreateFromAxisAngle(rotation_.Up(), -5.0f);
+
+	int r = Random::GetInstance().Range(0, 9);
+	if (r < probability_) {
+		//ターン
+		world_->getCanChangedScoreManager().addScore(playerNumber_, SCORE_TURN);
+		changeAnimation(Enemy_Animation::Turn, 0.0f, 1.0f, false);
+	}
+	else {
+		//クォーター
+		world_->getCanChangedScoreManager().addScore(playerNumber_, SCORE_QUARTER);
+		changeAnimation(Enemy_Animation::Quarter, 0.0f, 1.0f, false);
+	}
+}
+
+void Enemy_Notice::to_Normal() {
+	nextPoint_ = getNearestPoint(centerPosition_);
+	nextPosition_ = roundPoint_[nextPoint_];
+	isGoBonus_ = false;
+
+	if (data_->playerNumber_ != playerNumber_)
+		data_ = world_->getCanChangedScoreManager().getScoreData(playerNumber_);
+}
+
+void Enemy_Notice::to_Fever() {
+
+}
+
+int Enemy_Notice::getNearestPoint(const Vector3 & position) {
+	int result = 0;
+	for (int i = 0; i < roundPoint_.size(); i++) {
+		//対象ポイントが現在のポイントより近ければ
+		if (Vector3::Distance(position, roundPoint_[result]) > Vector3::Distance(position, roundPoint_[i])) {
+			//ポイントを更新
+			result = i;
+		}
+	}
+	return result;
+}
+
+void Enemy_Notice::setNextPosition() {
+	//ターゲットカウントを-1する
+	nextPoint_ = (nextPoint_ + roundPoint_.size() - 1) % roundPoint_.size();
+
+	float rate = 1.0f;
+	Vector3 nextPosition = world_->getCanChangedScoreMap().getNextPoint(centerPosition_, &rate);
+	if (rate >= 1.05f) {
+		isGoBonus_ = true;
+		nextPosition_ = nextPosition + Vector3(Random::GetInstance().Range(-20.f, 20.f), 0.0f, Random::GetInstance().Range(-20.f, 20.f));
+		return;
+	}
+	nextPosition_ = roundPoint_[nextPoint_];
+
+}
---
+++ MyGame/MyGame/src/Actor/Enemy/Enemy_Notice/Enemy_Notice.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include"../BaseEnemy.h"
+#include"../../../ScoreManager/ScoreData.h"
+
+class Enemy_Notice : public BaseEnemy{
+private:
+	enum Notice_State {
+		Normal,
+		Steal,
+	};
+public:
+	//コンストラクタ
+	Enemy_Notice(IWorld* world, const std::string& name, const Vector3& position, int playerNumber, const IBodyPtr& body = std::make_shared<BoundingCapsule>(Vector3(0.0f, 0.0f, 0.0f), Matrix::Identity, 20.0f, 3.0f));
+	// メッセージ処理
+	virtual void onMessage(EventMessage message, void* param) override;
+private:
+	//ステップ通知時の処理
+	virtual void JustStep()override;
+
+	virtual void updateNormal(float deltaTime) override;
+	virtual void updateFever(float deltaTime) override;
+
+	virtual void to_Normal() override;
+	virtual void to_Fever() override;
+private:
+	int getNearestPoint(const Vector3& position);
+
+	//次の目的地を設定する
+	void setNextPosition();
+private:
+	//ステイト
+	Notice_State state_;
+	//次のポイント
+	int nextPoint_;
+	//次の目的地
+	Vector3 nextPosition_;
+	//ボーナス
+	bool isGoBonus_{ false };
+	//ライトの座標
+	Vector3 lightPosition_;
+	//ステップをする確率
+	int probability_{ 4 };
+	//自分のデータ
+	ScoreData* data_;
+
+	//ステージ周回ポイント配列
+	std::vector<Vector3> roundPoint_;
+};
